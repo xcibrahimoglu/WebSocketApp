31fe8b5030072b2a9c459b10dab28a3beb2a5694
xcibrahimoglu <canibrahimoglu1@gmail.com>
2019-09-30 01:03:19 +0300

    some improvements in index.html

--- WebSocketApp/src/server/WebSocket.java
+++ WebSocketApp/src/server/WebSocket.java
@@ -1,6 +1,5 @@
 package server;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -24,46 +23,41 @@ import model.MessageDecoder;
 @ServerEndpoint(value = "/chat", encoders = MessageEncoder.class, decoders = MessageDecoder.class)
 public class WebSocket {
 	
-	static Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
+	static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
 	static Map<Session,String> connectedUserInfos = Collections.synchronizedMap(new HashMap<Session,String>());
     
     @OnOpen
     public void onOpen(Session session) {
         System.out.println("onOpen::" + session.getId());       
-        peers.add(session);
+        clients.add(session);
     }
+    
     @OnClose
     public void onClose(Session session) {
         System.out.println("onClose::" +  session.getId());
-        peers.remove(session);
+        clients.remove(session);
         connectedUserInfos.remove(session);
-        Set<String> connectedUserSet = new HashSet<String>(connectedUserInfos.values());
-		AllConnectedUsers allConnectedUsers = new AllConnectedUsers(connectedUserSet);
-		WebSocketMessage<AllConnectedUsers> webSocketMessage = new WebSocketMessage<AllConnectedUsers>(allConnectedUsers);
-		for(Session peer : peers){
-    		peer.getAsyncRemote().sendObject(webSocketMessage);
-        }
+        sendOnlineUsersToAllClients(connectedUserInfos);
     }
     
 	@SuppressWarnings("unchecked")
 	@OnMessage
-    public void handleTextMessage(Session session,@SuppressWarnings("rawtypes") WebSocketMessage webSocketMessage) {
-    	
+    public void handleMessage(Session session,@SuppressWarnings("rawtypes") WebSocketMessage webSocketMessage) {
+		Message textMessage = null;
     	if (webSocketMessage.getPayload() instanceof Message) {
-    		Message textMessage = (Message) webSocketMessage.getPayload();
+    		textMessage = (Message) webSocketMessage.getPayload();
    			webSocketMessage.setPayload(textMessage);
+   			
+   			for(Session client : clients){
+   	    		if(connectedUserInfos.get(client).equalsIgnoreCase(textMessage.getReceiver()) || connectedUserInfos.get(client).equalsIgnoreCase(textMessage.getSender()))
+   	    			client.getAsyncRemote().sendObject(webSocketMessage);
+   	        }
    		}
    		if(webSocketMessage.getPayload() instanceof ConnectedUser) { 
    			ConnectedUser newUser = (ConnectedUser) webSocketMessage.getPayload();
     		connectedUserInfos.put(session, newUser.getUsername());
-    		Set<String> connectedUserSet = new HashSet<String>(connectedUserInfos.values());
-    		AllConnectedUsers allConnectedUsers = new AllConnectedUsers(connectedUserSet);
-    		webSocketMessage = new WebSocketMessage<AllConnectedUsers>(allConnectedUsers);
-   		}
-    	for(Session peer : peers){
-    		peer.getAsyncRemote().sendObject(webSocketMessage);
-        }
-    	
+    		sendOnlineUsersToAllClients(connectedUserInfos);
+   		}    	
     }
      
     @OnMessage(maxMessageSize = 1024000)
@@ -75,5 +69,14 @@ public class WebSocket {
     @OnError
     public void onError(Throwable t) {
         System.out.println("onError::" + t.getMessage());
+    }
+    
+    public void sendOnlineUsersToAllClients(Map<Session,String> connectedUserInfos) {
+    	Set<String> connectedUserSet = new HashSet<String>(connectedUserInfos.values());
+		AllConnectedUsers allConnectedUsers = new AllConnectedUsers(connectedUserSet);
+		WebSocketMessage<AllConnectedUsers> webSocketMessage = new WebSocketMessage<AllConnectedUsers>(allConnectedUsers);
+		for(Session client : clients){
+			client.getAsyncRemote().sendObject(webSocketMessage);
+        }
     }
 }
